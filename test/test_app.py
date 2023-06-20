73a8a00fc8bbbe0af1351b4e97682138a32981b2
André Silva <andre15andre@hotmail.com>
2023-06-20 16:52:37 +0200

    initial implementation

---
+++ test/test_app.py
@@ -0,0 +1,31 @@
+from app.app import Calculator
+
+import unittest
+
+class TestCalculator(unittest.TestCase):
+    """
+    Test the sumInt function
+    """
+    def test_sumInt(self):
+        self.assertEqual(Calculator.sumInt(1, 1), 2)
+        self.assertEqual(Calculator.sumInt(1, 2), 3)
+        self.assertEqual(Calculator.sumInt(1, -1), 0)
+
+    """
+    Test the sumFloat function
+    """
+    def test_sumFloat(self):
+        self.assertEqual(Calculator.sumFloat(1.0, 1.0), 2.0)
+        self.assertEqual(Calculator.sumFloat(1.0, 2.0), 3.0)
+        self.assertEqual(Calculator.sumFloat(1.0, -1.0), 0.0)
+
+    # """
+    # Test the sum function
+    # """
+    # def test_sum(self):
+    #     self.assertEqual(Calculator.sum(1, 1), 2)
+    #     self.assertEqual(Calculator.sum(1, 2), 3)
+    #     self.assertEqual(Calculator.sum(1, -1), 0)
+    #     self.assertEqual(Calculator.sum(1.0, 1.0), 2.0)
+    #     self.assertEqual(Calculator.sum(1.0, 2.0), 3.0)
+    #     self.assertEqual(Calculator.sum(1.0, -1.0), 0.0)
